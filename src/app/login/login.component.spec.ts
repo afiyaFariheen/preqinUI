7ba4d437c3a48526f73440064302869f59a817d2
Afiya Fariheen <afiya.fariheen@maersk.com>
2023-12-03 23:18:53 +0530

    spec ts

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,14 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, inject, tick } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { AuthenticationService } from 'src/services/authentication.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let AuthenticationService:AuthenticationService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [
+        { provide: AuthenticationService, useValue: AuthenticationService },
+               ]
     })
     .compileComponents();
 
@@ -17,7 +21,13 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+ 
+
+  
+ 
 });
